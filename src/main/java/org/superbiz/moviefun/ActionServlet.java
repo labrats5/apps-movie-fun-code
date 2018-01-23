b854cf24f5fafc416392f52cffc33af0ef6c4471
Maxwell J. Corbin <Maxwell.Corbin@cognizant.com>
2018-01-23 14:00:35 -0700

    two data sources

--- src/main/java/org/superbiz/moviefun/ActionServlet.java
+++ src/main/java/org/superbiz/moviefun/ActionServlet.java
@@ -1,27 +1,12 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.superbiz.moviefun;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.superbiz.moviefun.movies.Movie;
 import org.superbiz.moviefun.movies.MoviesBean;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,8 +24,14 @@ public class ActionServlet extends HttpServlet {
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
-    private MoviesBean moviesBean;
+    private final MoviesBean moviesBean;
+    private final PlatformTransactionManager moviesTransactionManager;
+
+    public ActionServlet(MoviesBean moviesBean, PlatformTransactionManager moviesTransactionManager) {
+        this.moviesBean = moviesBean;
+        this.moviesTransactionManager = moviesTransactionManager;
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -64,18 +55,24 @@ public class ActionServlet extends HttpServlet {
             int year = Integer.parseInt(request.getParameter("year"));
 
             Movie movie = new Movie(title, director, genre, rating, year);
+            TransactionStatus transaction = moviesTransactionManager.getTransaction(null);
 
             moviesBean.addMovie(movie);
+
+            moviesTransactionManager.commit(transaction);
             response.sendRedirect("moviefun");
             return;
 
         } else if ("Remove".equals(action)) {
 
             String[] ids = request.getParameterValues("id");
+            TransactionStatus transaction = moviesTransactionManager.getTransaction(null);
+
             for (String id : ids) {
                 moviesBean.deleteMovieId(new Long(id));
             }
 
+            moviesTransactionManager.commit(transaction);
             response.sendRedirect("moviefun");
             return;
 
